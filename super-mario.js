b5778362744971ec4426035e962025d1b12fe440
icelolcream4-beep <icelolcream4@gmail.com>
2026-02-14 11:59:38 -0600

    trying to make it so when the mouse touches the square it gets set back

--- super-mario.js
+++ super-mario.js
@@ -8,20 +8,33 @@ let jumpFrame = 0;
 
 function setup() {
   createCanvas(600, 400);
+  r = 255
+  g = 60
+  b = 60
 }
 
 function draw() {
   background(120, 190, 255); // sky
+  fill(80,80,80);
+  rect(200,270,100,10)
+  fill(20,0,200)
+  rect(100,270,100,10)
 
   // ground
   fill(60, 200, 90);
   rect(0, 330, width, 70);
+  
+
   updateJump();
 
   drawPlayer();
 
-if (keyIsDown(65) === true) x = x - 5;
-if (keyIsDown(68) === true) x = x + 5;
+if (keyIsDown(65) === true) x = x - 10;
+if (keyIsDown(68) === true) x = x + 10;
+if (x >= 620) x = -30;
+if (x <= -35) x = 618;
+circle(mouseX,mouseY,10)
+if(mouseX - x == [0,10] && mouseY - y == [0,10]) x = 10;
 }
 // ==================================================
 // JUMP FUNCTION
@@ -58,6 +71,6 @@ function updateJump() {
 // 🎨 DRAW PLAYER
 // ==================================================
 function drawPlayer() {
-  fill(255, 60, 60);
+  fill(x+10, x/5, x/3);
   rect(x, y, 40, 40);
 }
